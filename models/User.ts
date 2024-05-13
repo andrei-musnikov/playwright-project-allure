c4d34e015c84cfc6b52555b3046516b7708ee245
Andrei Mušnikov <andrei.musnikov@outlook.com>
2024-05-13 16:45:29 +0200

    Modifying User model parameter to use parameter properties

--- models/User.ts
+++ models/User.ts
@@ -1,14 +1,14 @@
 import { faker } from "@faker-js/faker";
 
 export default class User {
-    private firstName: string;
-    private lastName: string;
-    private email: string;
-    private password: string;
-    private accessToken: string;
-    private userID: string;
-
-    constructor() {
+    constructor(
+        private firstName: string,
+        private lastName: string,
+        private email: string,
+        private password: string,
+        private accessToken: string,
+        private userID: string
+    ) {
         this.firstName = faker.person.firstName();
         this.lastName = faker.person.lastName();
         this.email = faker.internet.email();
